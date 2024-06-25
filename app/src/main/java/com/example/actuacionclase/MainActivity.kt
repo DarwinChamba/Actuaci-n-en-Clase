3e3b76f604aeb9cd26bc496b8b5770fdc80b2ba6
Jose Mosquera <jose-14mosquera@hotmail.com>
2024-06-24 19:39:06 -0500

    comportamiento logico

--- app/src/main/java/com/example/actuacionclase/MainActivity.kt
+++ app/src/main/java/com/example/actuacionclase/MainActivity.kt
@@ -4,33 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
-import com.example.actuacionclase.util.User
+import com.example.actuacionclase.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var btnAdd:Button
-    private lateinit var imprimirLista:Button
+
     private var id=0
+    private lateinit var biding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        btnAdd=findViewById(R.id.agregarUsuario)
-        imprimirLista=findViewById(R.id.imprimirLista)
-        btnAdd.setOnClickListener {
-            addUser()
-        }
-        imprimirLista.setOnClickListener {
-            imprimir()
-        }
-    }
-    private fun addUser(){
+        biding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(biding.root)
 
-        val user= User(id ++,"Darwin",6f,"")
-        Data.lista.add(user)
-        Toast.makeText(this,"Usuario regustrado con exito",Toast.LENGTH_SHORT).show()
-    }
-    private  fun imprimir(){
-        Data.lista.forEach {
-            println("${it.nombre} ${it.calificacion} ${it.nota} ${it.id}")
+        biding.ingresar.setOnClickListener{ Toast.makeText(this, "cualquier mensaje", Toast.LENGTH_SHORT).show()}
         }
+
     }
-}
