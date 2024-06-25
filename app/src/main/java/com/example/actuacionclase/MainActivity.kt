9f0ca22434cf0f24fb6648375e701c67193f22a4
Marcelo Torres <kevin.torres85@outlook.es>
2024-06-24 19:43:11 -0500

    corregido

--- app/src/main/java/com/example/actuacionclase/MainActivity.kt
+++ app/src/main/java/com/example/actuacionclase/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.actuacionclase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.actuacionclase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var id=0
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.boton1.setOnClickListener {Toast.makeText(this,"Validado",Toast.LENGTH_SHORT).show()}
     }
 }
